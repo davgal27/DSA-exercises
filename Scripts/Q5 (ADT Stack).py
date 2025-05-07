f9bda677411a39016ca2f7ff3b40d6832415bd87
davgal27 <davegalea27@gmail.com>
2025-05-07 14:45:47 +0200

    Finished Q 5

--- Scripts/Q5 (ADT Stack).py
+++ Scripts/Q5 (ADT Stack).py
@@ -1,18 +1,29 @@
+# Write a program that uses an ADT Stack to evaluate arithmetic expressions in RPN
+# format. The contents of the stack should be displayed on the screen during
+# evaluation. The allowed arithmetic operators are +, -, x, and /.
+
 def evaluate_rpn(expr):
     stack = []
-    tokens = expr.split()
-    for token in tokens:
-        if token.isdigit():
-            stack.append(int(token))
-        else:
-            b, a = stack.pop(), stack.pop()
-            if token == '+': result = a + b
-            elif token == '-': result = a - b
-            elif token == 'x': result = a * b
-            elif token == '/': result = a / b
+
+    for item in expr.split():
+        if item.isdigit(): #If the item from the split is a number
+            stack.append(float(item)) # Add the item to the stack
+        else: #If the item is not a number (operand)
+            b = stack.pop() # Popping in LIFO format
+            a = stack.pop()
+            print(f"Operation: {a} {item} {b}")
+            if item == '+':
+                result = a + b
+            elif item == '-':
+                result = a - b
+            elif item == 'x':
+                result = a * b
+            elif item == '/':
+                result = a / b
+            result  = round(result, 2)
             stack.append(result)
         print("Stack:", stack)
     return stack[0]
 
-# Example usage
-# evaluate_rpn("3 4 + 2 x 7 /")
+evaluate_rpn("4 6 + 2 x 3 / 8 - 10 +")
+
